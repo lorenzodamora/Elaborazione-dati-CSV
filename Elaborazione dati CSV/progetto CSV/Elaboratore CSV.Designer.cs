6b6b82ccb60a3ad844787ba5865213a2e8020472
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-05-08 00:04:20 +0200

    Aggiungere i file di progetto.

---
+++ Elaborazione dati CSV/progetto CSV/Elaboratore CSV.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows.Forms;
+
+namespace Elaborazione_dati_CSV
+{
+    partial class Elaboratore_CSV
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GraphicTitle = new System.Windows.Forms.Label();
+            this.ListaProdotti = new System.Windows.Forms.ListView();
+            this.NameList = new System.Windows.Forms.TextBox();
+            this.ChiudiFormButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GraphicTitle
+            // 
+            this.GraphicTitle.BackColor = System.Drawing.Color.Black;
+            this.GraphicTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.30189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GraphicTitle.ForeColor = System.Drawing.Color.White;
+            this.GraphicTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GraphicTitle.Location = new System.Drawing.Point(424, 9);
+            this.GraphicTitle.Name = "GraphicTitle";
+            this.GraphicTitle.Size = new System.Drawing.Size(400, 40);
+            this.GraphicTitle.TabIndex = 15;
+            this.GraphicTitle.Text = "ELABORATORE DATI CSV";
+            this.GraphicTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ListaProdotti
+            // 
+            this.ListaProdotti.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListaProdotti.HideSelection = false;
+            this.ListaProdotti.Location = new System.Drawing.Point(830, 75);
+            this.ListaProdotti.Name = "ListaProdotti";
+            this.ListaProdotti.Size = new System.Drawing.Size(930, 870);
+            this.ListaProdotti.TabIndex = 16;
+            this.ListaProdotti.UseCompatibleStateImageBehavior = false;
+            this.ListaProdotti.View = System.Windows.Forms.View.List;
+            // 
+            // NameList
+            // 
+            this.NameList.BackColor = System.Drawing.Color.White;
+            this.NameList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NameList.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.NameList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameList.ForeColor = System.Drawing.Color.Black;
+            this.NameList.Location = new System.Drawing.Point(830, 46);
+            this.NameList.MaxLength = 100;
+            this.NameList.Name = "NameList";
+            this.NameList.ReadOnly = true;
+            this.NameList.Size = new System.Drawing.Size(930, 24);
+            this.NameList.TabIndex = 25;
+            this.NameList.Text = "Non è stata selezionata nessuna linea";
+            // 
+            // ChiudiFormButton
+            // 
+            this.ChiudiFormButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ChiudiFormButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChiudiFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChiudiFormButton.Location = new System.Drawing.Point(1758, 1);
+            this.ChiudiFormButton.Name = "ChiudiFormButton";
+            this.ChiudiFormButton.Size = new System.Drawing.Size(25, 25);
+            this.ChiudiFormButton.TabIndex = 27;
+            this.ChiudiFormButton.UseVisualStyleBackColor = false;
+            // 
+            // Elaboratore_CSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1784, 959);
+            this.Controls.Add(this.ChiudiFormButton);
+            this.Controls.Add(this.NameList);
+            this.Controls.Add(this.ListaProdotti);
+            this.Controls.Add(this.GraphicTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Elaboratore_CSV";
+            this.Text = "Lista CRUD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label GraphicTitle;
+        private System.Windows.Forms.ListView ListaProdotti;
+        private System.Windows.Forms.TextBox NameList;
+        private System.Windows.Forms.Button ChiudiFormButton;
+    }
+}
+
---
+++ Elaborazione dati CSV/progetto CSV/Elaboratore CSV.cs
@@ -0,0 +1,29 @@
+﻿#region using
+using System;
+/* using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
+using System.Data;
+using System.Linq; //ha .ToArray();
+using System.Net.Http;
+using System.Threading; */
+//using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text;
+using System.Diagnostics;
+
+namespace Elaborazione_dati_CSV
+{
+	public partial class Elaboratore_CSV : Form
+	{
+		#endregion
+		public Elaboratore_CSV()
+		{
+			InitializeComponent();
+		}
+	}
+}
