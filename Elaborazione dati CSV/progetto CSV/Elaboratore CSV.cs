856dd8e535a9496401eecfaa9f181aacd5a191a0
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-05-26 12:08:38 +0200

    punto 1. pt1

--- Elaborazione dati CSV/progetto CSV/Elaboratore CSV.cs
+++ Elaborazione dati CSV/progetto CSV/Elaboratore CSV.cs
@@ -29,27 +29,28 @@ namespace Elaborazione_dati_CSV
 	}*/
 	public partial class Elaboratore_CSV : Form
 	{
-		//Aggiungere in coda ad ogni record un campo chiamato miovalore contenente un numero casuale 10<=X<=20 + un campo per la cancellazione logica
-		//contare il numero dei campi che compongono il record
-		//calcolare la lunghezza massima dei record presenti indicando anche la lunghezza massima di ogni campo
-		//inserire in ogni record un numero di spazi necessari a rendere fissa la dimensione di tutti i record, senza perdere informazioni
-		//Aggiungere un record in coda
-		//Visualizzare dei dati mostrando tre campi significativi a scelta
-		//Ricercare un record per campo chiave a scelta (se esiste, utilizzare il campo che contiene dati univoci
-		//Modificare un record
-		//Cancellare logicamente un record
-		//Realizzare l'interfaccia grafica che consenta l'interazione fluida con le funzionalità descritte. Richiamare le funzioni di servizio dalle funzioni di gestione degli eventi
+		//1. Aggiungere in coda ad ogni record un campo chiamato miovalore contenente un numero casuale 10<=X<=20 + un campo per la cancellazione logica
+		//2. contare il numero dei campi che compongono il record
+		//3. calcolare la lunghezza massima dei record presenti indicando anche la lunghezza massima di ogni campo
+		//4. inserire in ogni record un numero di spazi necessari a rendere fissa la dimensione di tutti i record, senza perdere informazioni
+		//5. Aggiungere un record in coda
+		//6. Visualizzare dei dati mostrando tre campi significativi a scelta
+		//7. Ricercare un record per campo chiave a scelta (se esiste, utilizzare il campo che contiene dati univoci
+		//8. Modificare un record
+		//9. Cancellare logicamente un record
+		//10. Realizzare l'interfaccia grafica che consenta l'interazione fluida con le funzionalità descritte. Richiamare le funzioni di servizio dalle funzioni di gestione degli eventi
 
 		//divisione in pagine (42 linee per pagina)
 
 		public string path;
 		public int totline; //tutte le linee tranne header
-		public string[] csvLines;
+		public string[] csvLines; //per non rileggere ogni volta
 		public Elaboratore_CSV()
 		{
 			InitializeComponent();
 			path = Path.GetFullPath("..\\..\\..") + "\\files\\damora.csv";
 			totline = GetLineCount(path) -1;
+			//continua in shown
 		}
 		private int GetLineCount(string path)
 		{
@@ -97,11 +98,11 @@ namespace Elaborazione_dati_CSV
 		private void FileWriteAllLines(string path, string[] lines)
 		{
 			FileWriteAllLines(path, lines, FileMode.Create);
-		}
+		} */
 		private void FileWriteAllLines(string path, string[] lines, FileMode mode)
 		{
 			FileWriteAllText(path, string.Join("\n", lines), mode);
-		}*/
+		}
 
 		private void StampaCSV(string[] lines)
 		{
@@ -127,11 +128,28 @@ namespace Elaborazione_dati_CSV
 		{
 			//SetVisible();
 			csvLines = FileReadAllLines(path);
+			AddCampo(csvLines,path);
 			StampaCSV(csvLines);
 		}
 		private void Shortcut(object sender, KeyEventArgs e)
 		{
 
+		}
+
+		private void AddCampo(string[] csvLines, string path)
+		{
+			string[] split = csvLines[0].Split(';');
+			for(int i = 0; i < split.Length; i++)
+				if(split[i] == "miovalore") return;
+
+			csvLines[0] += ";miovalore";
+			Random rnd;
+			for(int i = 1; i < csvLines.Length; i++)
+			{
+				rnd = new Random();
+				csvLines[i] += $";{rnd.Next(10,20+1)}";
+			}
+			FileWriteAllLines(path, csvLines, FileMode.Open);
 		}
 	}
 }
