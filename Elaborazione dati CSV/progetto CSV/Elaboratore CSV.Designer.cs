9240d95e21000624d69685f0ab6025965fa24c2e
damora computer scuola i2-13 pc6 <damora.20465@LABI2-13PC6>
2023-05-26 13:55:16 +0200

    punto 3. pt1

--- Elaborazione dati CSV/progetto CSV/Elaboratore CSV.Designer.cs
+++ Elaborazione dati CSV/progetto CSV/Elaboratore CSV.Designer.cs
@@ -30,85 +30,119 @@ namespace Elaborazione_dati_CSV
         /// </summary>
         private void InitializeComponent()
         {
-			this.GraphicTitle = new System.Windows.Forms.Label();
-			this.Lista = new System.Windows.Forms.ListView();
-			this.NameList = new System.Windows.Forms.TextBox();
-			this.ChiudiFormButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// GraphicTitle
-			// 
-			this.GraphicTitle.BackColor = System.Drawing.Color.Black;
-			this.GraphicTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.30189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.GraphicTitle.ForeColor = System.Drawing.Color.White;
-			this.GraphicTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-			this.GraphicTitle.Location = new System.Drawing.Point(424, 9);
-			this.GraphicTitle.Name = "GraphicTitle";
-			this.GraphicTitle.Size = new System.Drawing.Size(400, 40);
-			this.GraphicTitle.TabIndex = 15;
-			this.GraphicTitle.Text = "ELABORATORE DATI CSV";
-			this.GraphicTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// Lista
-			// 
-			this.Lista.AllowColumnReorder = true;
-			this.Lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.Lista.FullRowSelect = true;
-			this.Lista.GridLines = true;
-			this.Lista.HideSelection = false;
-			this.Lista.Location = new System.Drawing.Point(830, 75);
-			this.Lista.Name = "Lista";
-			this.Lista.Size = new System.Drawing.Size(930, 870);
-			this.Lista.TabIndex = 16;
-			this.Lista.UseCompatibleStateImageBehavior = false;
-			this.Lista.View = System.Windows.Forms.View.Details;
-			// 
-			// NameList
-			// 
-			this.NameList.BackColor = System.Drawing.Color.White;
-			this.NameList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.NameList.Cursor = System.Windows.Forms.Cursors.Arrow;
-			this.NameList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.NameList.ForeColor = System.Drawing.Color.Black;
-			this.NameList.Location = new System.Drawing.Point(830, 46);
-			this.NameList.MaxLength = 100;
-			this.NameList.Name = "NameList";
-			this.NameList.ReadOnly = true;
-			this.NameList.Size = new System.Drawing.Size(930, 24);
-			this.NameList.TabIndex = 25;
-			this.NameList.Text = "Non è stata selezionata nessuna linea";
-			// 
-			// ChiudiFormButton
-			// 
-			this.ChiudiFormButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-			this.ChiudiFormButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.ChiudiFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ChiudiFormButton.Location = new System.Drawing.Point(1758, 1);
-			this.ChiudiFormButton.Name = "ChiudiFormButton";
-			this.ChiudiFormButton.Size = new System.Drawing.Size(25, 25);
-			this.ChiudiFormButton.TabIndex = 27;
-			this.ChiudiFormButton.UseVisualStyleBackColor = false;
-			// 
-			// Elaboratore_CSV
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ControlDark;
-			this.ClientSize = new System.Drawing.Size(1784, 959);
-			this.Controls.Add(this.ChiudiFormButton);
-			this.Controls.Add(this.NameList);
-			this.Controls.Add(this.Lista);
-			this.Controls.Add(this.GraphicTitle);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.KeyPreview = true;
-			this.MaximizeBox = false;
-			this.Name = "Elaboratore_CSV";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-			this.Text = "Lista CRUD";
-			this.Shown += new System.EventHandler(this.Form_Shown);
-			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Shortcut);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.GraphicTitle = new System.Windows.Forms.Label();
+            this.Lista = new System.Windows.Forms.ListView();
+            this.NameList = new System.Windows.Forms.TextBox();
+            this.ChiudiFormButton = new System.Windows.Forms.Button();
+            this.TotFieldBox = new System.Windows.Forms.TextBox();
+            this.MaxLengthBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GraphicTitle
+            // 
+            this.GraphicTitle.BackColor = System.Drawing.Color.Black;
+            this.GraphicTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.30189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GraphicTitle.ForeColor = System.Drawing.Color.White;
+            this.GraphicTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GraphicTitle.Location = new System.Drawing.Point(424, 9);
+            this.GraphicTitle.Name = "GraphicTitle";
+            this.GraphicTitle.Size = new System.Drawing.Size(400, 40);
+            this.GraphicTitle.TabIndex = 15;
+            this.GraphicTitle.Text = "ELABORATORE DATI CSV";
+            this.GraphicTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lista
+            // 
+            this.Lista.AllowColumnReorder = true;
+            this.Lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lista.FullRowSelect = true;
+            this.Lista.GridLines = true;
+            this.Lista.HideSelection = false;
+            this.Lista.Location = new System.Drawing.Point(830, 75);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(930, 870);
+            this.Lista.TabIndex = 16;
+            this.Lista.UseCompatibleStateImageBehavior = false;
+            this.Lista.View = System.Windows.Forms.View.Details;
+            // 
+            // NameList
+            // 
+            this.NameList.BackColor = System.Drawing.Color.White;
+            this.NameList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NameList.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.NameList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameList.ForeColor = System.Drawing.Color.Black;
+            this.NameList.Location = new System.Drawing.Point(830, 46);
+            this.NameList.MaxLength = 100;
+            this.NameList.Name = "NameList";
+            this.NameList.ReadOnly = true;
+            this.NameList.Size = new System.Drawing.Size(930, 23);
+            this.NameList.TabIndex = 25;
+            this.NameList.Text = "Non è stata selezionata nessuna linea";
+            // 
+            // ChiudiFormButton
+            // 
+            this.ChiudiFormButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ChiudiFormButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChiudiFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChiudiFormButton.Location = new System.Drawing.Point(1758, 1);
+            this.ChiudiFormButton.Name = "ChiudiFormButton";
+            this.ChiudiFormButton.Size = new System.Drawing.Size(25, 25);
+            this.ChiudiFormButton.TabIndex = 27;
+            this.ChiudiFormButton.UseVisualStyleBackColor = false;
+            // 
+            // TotFieldBox
+            // 
+            this.TotFieldBox.BackColor = System.Drawing.Color.White;
+            this.TotFieldBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotFieldBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TotFieldBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotFieldBox.ForeColor = System.Drawing.Color.Black;
+            this.TotFieldBox.Location = new System.Drawing.Point(566, 75);
+            this.TotFieldBox.MaxLength = 100;
+            this.TotFieldBox.Name = "TotFieldBox";
+            this.TotFieldBox.ReadOnly = true;
+            this.TotFieldBox.Size = new System.Drawing.Size(258, 23);
+            this.TotFieldBox.TabIndex = 28;
+            this.TotFieldBox.Text = "Numero di campi: ";
+            // 
+            // MaxLengthBox
+            // 
+            this.MaxLengthBox.BackColor = System.Drawing.Color.White;
+            this.MaxLengthBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MaxLengthBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaxLengthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxLengthBox.ForeColor = System.Drawing.Color.Black;
+            this.MaxLengthBox.Location = new System.Drawing.Point(566, 104);
+            this.MaxLengthBox.MaxLength = 100;
+            this.MaxLengthBox.Name = "MaxLengthBox";
+            this.MaxLengthBox.ReadOnly = true;
+            this.MaxLengthBox.Size = new System.Drawing.Size(258, 23);
+            this.MaxLengthBox.TabIndex = 29;
+            this.MaxLengthBox.Text = "lunghezza massima dei record: ";
+            // 
+            // Elaboratore_CSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1784, 959);
+            this.Controls.Add(this.MaxLengthBox);
+            this.Controls.Add(this.TotFieldBox);
+            this.Controls.Add(this.ChiudiFormButton);
+            this.Controls.Add(this.NameList);
+            this.Controls.Add(this.Lista);
+            this.Controls.Add(this.GraphicTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Elaboratore_CSV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Lista CRUD";
+            this.Shown += new System.EventHandler(this.Form_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Shortcut);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,6 +151,8 @@ namespace Elaborazione_dati_CSV
         private System.Windows.Forms.ListView Lista;
         private System.Windows.Forms.TextBox NameList;
         private System.Windows.Forms.Button ChiudiFormButton;
+        private TextBox TotFieldBox;
+        private TextBox MaxLengthBox;
     }
 }
 
--- Elaborazione dati CSV/progetto CSV/Elaboratore CSV.cs
+++ Elaborazione dati CSV/progetto CSV/Elaboratore CSV.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 
 namespace Elaborazione_dati_CSV
 {
-	/*public struct StructLine
+    /*public struct StructLine
 	{
 		public int ind;
 		public string[] splits;
@@ -27,70 +27,70 @@ namespace Elaborazione_dati_CSV
 		public StructLine[] csvLines;
 		public int totline; //totline totale linee
 	}*/
-	public partial class Elaboratore_CSV : Form
-	{
-		//x 1. Aggiungere in coda ad ogni record un campo chiamato miovalore contenente un numero casuale 10<=X<=20 + un campo per la cancellazione logica
-		//2. contare il numero dei campi che compongono il record
-		//3. calcolare la lunghezza massima dei record presenti indicando anche la lunghezza massima di ogni campo
-		//4. inserire in ogni record un numero di spazi necessari a rendere fissa la dimensione di tutti i record, senza perdere informazioni
-		//5. Aggiungere un record in coda
-		//6. Visualizzare dei dati mostrando tre campi significativi a scelta
-		//7. Ricercare un record per campo chiave a scelta (se esiste, utilizzare il campo che contiene dati univoci
-		//8. Modificare un record
-		//9. Cancellare logicamente un record
-		//10. Realizzare l'interfaccia grafica che consenta l'interazione fluida con le funzionalità descritte. Richiamare le funzioni di servizio dalle funzioni di gestione degli eventi
+    public partial class Elaboratore_CSV : Form
+    {
+        //x 1. Aggiungere in coda ad ogni record un campo chiamato miovalore contenente un numero casuale 10<=X<=20 + un campo per la cancellazione logica
+        //x 2. contare il numero dei campi che compongono il record
+        //3. calcolare la lunghezza massima dei record presenti indicando anche la lunghezza massima di ogni campo
+        //4. inserire in ogni record un numero di spazi necessari a rendere fissa la dimensione di tutti i record, senza perdere informazioni
+        //5. Aggiungere un record in coda
+        //6. Visualizzare dei dati mostrando tre campi significativi a scelta
+        //7. Ricercare un record per campo chiave a scelta (se esiste, utilizzare il campo che contiene dati univoci
+        //8. Modificare un record
+        //9. Cancellare logicamente un record
+        //10. Realizzare l'interfaccia grafica che consenta l'interazione fluida con le funzionalità descritte. Richiamare le funzioni di servizio dalle funzioni di gestione degli eventi
 
-		//divisione in pagine (42 linee per pagina)
+        //divisione in pagine (42 linee per pagina)
 
-		public string path;
-		public int totline, totfield; //tutte le linee tranne header //totale campi
-		public string[] csvLines; //per non rileggere ogni volta
-		public Elaboratore_CSV()
-		{
-			InitializeComponent();
-			path = Path.GetFullPath("..\\..\\..") + "\\files\\damora.csv";
-			totline = GetLineCount(path) -1;
-			//continua in shown
-		}
-		private int GetLineCount(string path)
-		{
-			int lineCount = 0;
-			FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
-			for(int ch = fs.ReadByte(); ch!=-1; ch = fs.ReadByte())
-				if((char)ch == '\n') lineCount++;
-			fs.Close();
-			return lineCount;
-		}
+        public string path;
+        public int totline, totfield; //tutte le linee tranne header //totale campi
+        public string[] csvLines; //per non rileggere ogni volta
+        public Elaboratore_CSV()
+        {
+            InitializeComponent();
+            path = Path.GetFullPath("..\\..\\..") + "\\files\\damora.csv";
+            totline = GetLineCount(path) - 1;
+            //continua in shown
+        }
+        private int GetLineCount(string path)
+        {
+            int lineCount = 0;
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+            for (int ch = fs.ReadByte(); ch != -1; ch = fs.ReadByte())
+                if ((char)ch == '\n') lineCount++;
+            fs.Close();
+            return lineCount;
+        }
 
-		private string[] FileReadAllLines(string path)
-		{
-			byte[] b = new byte[1024];
-			UTF8Encoding temp = new UTF8Encoding(true);
-			string line = "";
-			FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
-			for(int l; (l = fs.Read(b, 0, b.Length)) > 0;)
-				line += temp.GetString(b, 0, l);
-			fs.Close();
+        private string[] FileReadAllLines(string path)
+        {
+            byte[] b = new byte[1024];
+            UTF8Encoding temp = new UTF8Encoding(true);
+            string line = "";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+            for (int l; (l = fs.Read(b, 0, b.Length)) > 0;)
+                line += temp.GetString(b, 0, l);
+            fs.Close();
 
-			if(line == "") return new string[0];
-			string[] lines;
-			if(line[line.Length-1] == '\n')
-				// .SubString() perché altrimenti per ultimo rimarrebbe una stringa vuota
-				lines = line.Substring(0, line.Length-1).Split('\n');
-			else
-				lines = line.Split('\n');
-			for(int i = 0; i < lines.Length; i++)
-				lines[i] = lines[i].TrimEnd('\r');
-			return lines;
-		}
-		private void FileWriteAllText(string path, string allLines, FileMode mode)
-		{
-			Byte[] info = new UTF8Encoding(true).GetBytes(allLines);
-			FileStream fs = new FileStream(path, mode, FileAccess.Write, FileShare.None);
-			fs.Write(info, 0, info.Length);
-			fs.Close();
-		}
-		/* funzioni non utilizzate
+            if (line == "") return new string[0];
+            string[] lines;
+            if (line[line.Length - 1] == '\n')
+                // .SubString() perché altrimenti per ultimo rimarrebbe una stringa vuota
+                lines = line.Substring(0, line.Length - 1).Split('\n');
+            else
+                lines = line.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+                lines[i] = lines[i].TrimEnd('\r');
+            return lines;
+        }
+        private void FileWriteAllText(string path, string allLines, FileMode mode)
+        {
+            Byte[] info = new UTF8Encoding(true).GetBytes(allLines);
+            FileStream fs = new FileStream(path, mode, FileAccess.Write, FileShare.None);
+            fs.Write(info, 0, info.Length);
+            fs.Close();
+        }
+        /* funzioni non utilizzate
 		private void FileWriteAllText(string path, string allLines)
 		{
 			FileWriteAllText(path, allLines, FileMode.Create);
@@ -99,63 +99,88 @@ namespace Elaborazione_dati_CSV
 		{
 			FileWriteAllLines(path, lines, FileMode.Create);
 		} */
-		private void FileWriteAllLines(string path, string[] lines, FileMode mode)
-		{
-			FileWriteAllText(path, string.Join("\n", lines), mode);
-		}
-
-		private void StampaCSV(string[] lines)
-		{
-			Lista.Clear();
-			string[] splits;
-			ListViewItem line;
-			for(int i = 1; i < lines.Length; i++)
-			{
-				splits = lines[i].Split(';');
-				line = new ListViewItem(i.ToString()); //main item
-				for(int j = 0; j < splits.Length; j++)
-					line.SubItems.Add(splits[j]); //sub item
-				Lista.Items.Add(line);
-			}
-			splits = lines[0].Split(';');
-			Lista.Columns.Add("line", -2, HorizontalAlignment.Center);
-			for(int i = 0; i < splits.Length; i++)
-				Lista.Columns.Add(splits[i], -2, HorizontalAlignment.Center);
-			Lista.Columns.Add("", -2);
-		}
-
-		private void Form_Shown(object sender, EventArgs e)
-		{
-			//SetVisible();
-			csvLines = FileReadAllLines(path);
-			AddCampo(csvLines,path);
-			totfield = GetTotFields(csvLines[0]);
-			StampaCSV(csvLines);
-		}
-		private void Shortcut(object sender, KeyEventArgs e)
-		{
-
-		}
-
-		private void AddCampo(string[] csvLines, string path)
-		{
-			string[] split = csvLines[0].Split(';');
-			for(int i = 0; i < split.Length; i++)
-				if(split[i] == "miovalore") return;
-
-			csvLines[0] += ";miovalore;logic";
-			Random rnd;
-			for(int i = 1; i < csvLines.Length; i++)
-			{
-				rnd = new Random();
-				csvLines[i] += $";{rnd.Next(10,20+1)};0";
-			}
-			FileWriteAllLines(path, csvLines, FileMode.Open);
-		}
-        private int GetTotFields(string csvLine)
-		{
-			return csvLine.Split(';').Length;
+        private void FileWriteAllLines(string path, string[] lines, FileMode mode)
+        {
+            FileWriteAllText(path, string.Join("\n", lines), mode);
         }
 
+        private void StampaCSV(string[] lines)
+        {
+            Lista.Clear();
+            string[] splits;
+            ListViewItem line;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                splits = lines[i].Split(';');
+                line = new ListViewItem(i.ToString()); //main item
+                for (int j = 0; j < splits.Length; j++)
+                    line.SubItems.Add(splits[j]); //sub item
+                Lista.Items.Add(line);
+            }
+            splits = lines[0].Split(';');
+            ColumnHeader[] ch = new ColumnHeader[splits.Length+1];
+            ch[0] = new ColumnHeader
+            {
+                Text = "line",
+                Width = -2,
+                TextAlign = HorizontalAlignment.Center
+            };
+            for (int i = 0; i < splits.Length; i++)
+                ch[i+1] = new ColumnHeader
+                {
+                    Text = splits[i],
+                    Width = -2,
+                    TextAlign = HorizontalAlignment.Center
+                };
+            Lista.Columns.AddRange(ch);
+            for (int i = 0; i < splits.Length; i++)
+            {
+                ch[i].AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+        }
+
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            //SetVisible();
+            csvLines = FileReadAllLines(path);
+            AddCampo(csvLines, path);
+            totfield = GetTotFields(csvLines[0]);
+            TotFieldBox.Text += totfield - 1;
+            MaxLengthBox.Text += GetMaxLength(csvLines);
+            StampaCSV(csvLines);
+        }
+        private void Shortcut(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void AddCampo(string[] csvLines, string path)
+        {
+            string[] split = csvLines[0].Split(';');
+            for (int i = 0; i < split.Length; i++)
+                if (split[i] == "miovalore") return;
+
+            csvLines[0] += ";miovalore;logic";
+            Random rnd;
+            for (int i = 1; i < csvLines.Length; i++)
+            {
+                rnd = new Random(i);
+                csvLines[i] += $";{rnd.Next(10, 20 + 1)};0";
+            }
+            FileWriteAllLines(path, csvLines, FileMode.Open);
+        }
+        private int GetTotFields(string csvLine)
+        {
+            return csvLine.Split(';').Length;
+        }
+        private int GetMaxLength(string[] csvLines)
+        {
+            int max = 0;
+            for (int i = 0; i < csvLines.Length; i++)
+            {
+                if (csvLines[i].Length > max) max = csvLines[i].Length;
+            }
+            return max;
+        }
     }
 }
