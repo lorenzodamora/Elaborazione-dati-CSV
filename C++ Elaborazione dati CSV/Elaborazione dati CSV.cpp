3b886adb0754f76959da3cbe92e5789ccb3477b4
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-09-13 18:27:51 +0200

    full screen

--- C++ Elaborazione dati CSV/Elaborazione dati CSV.cpp
+++ C++ Elaborazione dati CSV/Elaborazione dati CSV.cpp
@@ -1,20 +1,32 @@
-// Elaborazione dati CSV.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
 #include <iostream>
+#include <Windows.h> // full screen
 
-int main()
+using namespace std;
+
+/**
+ * Designer Console. Crea le grafiche iniziali.
+ * @return True se non ha trovato la finestra da mettere full screen.
+ */
+bool InitializeConsole()
 {
-    std::cout << "Hello World!\n";
+	/** Full Screen */
+	HWND hWnd = GetConsoleWindow();
+	if (hWnd != NULL) {
+		ShowWindow(hWnd, 3); // Usato invece di SW_SHOWMAXIMIZED
+	}
+	else {
+		return true;
+	}
+
+	return false;
 }
 
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
+void main()
+{
+	cout << "Hello World!\n";
 
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
+	if (InitializeConsole()) {
+		cout << "error in InitializeConsole() \n\n\n\n\n\n\n\n";
+		return;
+	}
+}
